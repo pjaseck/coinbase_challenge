df8296ac8e5fca08989e411e253dc89c107f85d0
Katarzyna M <kmpiasek@gmail.com>
2023-09-23 20:21:23 +0200

    objects documentation

--- obj/order.py
+++ obj/order.py
@@ -1,4 +1,22 @@
 class Order:
+    """
+    A class used to represent an order details.
+
+    Attributes
+    ----------
+    price : str
+        the price of the transaction (buy or sell)
+    quantity : str
+        a quantity of the product that can be purchased/selled for the price
+    count : str
+        a number of transactions at this price level
+
+    Methods
+    -------
+    from_json
+        Converts response in json file into callable Order object.
+
+    """
     def __init__(self, price, quantity, count):
         self.price = price
         self.quantity = quantity
--- obj/product_book.py
+++ obj/product_book.py
@@ -1,23 +1,58 @@
 from obj.order import Order
 
 class ProductBook:
-    def __init__(self, bids, asks, sequence, auction_mode, auction, time):
+    """
+    A class used to represent a product book.
+
+    Attributes
+    ----------
+    bids : array
+        a list containing best bid details (price, quantity and count)
+    asks : array
+        a list containing best bid details (price, quantity and count)
+    sequence : str
+        unique number for the order book response
+    time : str
+        product book time
+
+    Methods
+    -------
+    from_json
+        Converts response in json file into callable ProductBook object.
+
+    """
+    def __init__(self, bids, asks, sequence, time):
+        """
+        Parameters
+        ----------
+        bids : array
+            a list containing best bid details
+        asks : array
+            a list containing best bid details
+        sequence : str
+            unique number for the order book response
+        time : str
+            product book time
+        """
         self.bids = bids
         self.asks = asks
         self.sequence = sequence
-        self.auction_mode = auction_mode
-        self.auction = auction
         self.time = time
     
     @staticmethod
     def from_json(json_data):
+        """Converts response in json file into callable ProductBook object.
+
+        Returns
+        -------
+        class
+            ProductBook object
+        """
         bids = Order.from_json(json_data['bids'])
         asks = Order.from_json(json_data['asks'])
         return ProductBook(
             bids,
             asks,
             json_data['sequence'],
-            json_data['auction_mode'],
-            json_data['auction'],
             json_data['time']
         )
